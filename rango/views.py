d71f28affd72c1f46bdbc641493e365460a48e96
NY <ny@NYs-MacBook-Pro.local>
2021-07-29 23:20:34 +0100

    Add cookies on Chapter 10

--- rango/views.py
+++ rango/views.py
@@ -16,12 +16,14 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     context_dict['extra'] = 'I LOVE CATS!'
-
+    request.session.set_test_cookie()
     return render(request, 'rango/index.html', context=context_dict)
 
 
 def about(request):
-
+    if request.session.test_cookie_worked():
+        print("TEST COOKIE WORKED!")
+        request.session.delete_test_cookie()
     return render(request, 'rango/about.html')
 
 
